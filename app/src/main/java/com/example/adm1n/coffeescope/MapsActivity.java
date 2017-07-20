0a0a30c76dce019011074efa9c8e95a9ca8ca77a
sergey <kudishov@inbox.ru>
2017-07-20 18:40:21 +0300

    обновлена моделька превью карточки

--- app/src/main/java/com/example/adm1n/coffeescope/MapsActivity.java
+++ app/src/main/java/com/example/adm1n/coffeescope/MapsActivity.java
@@ -15,6 +15,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,38 +27,52 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private final LatLng DEFAULT_RED_SQUARE = new LatLng(55.753922, 37.620783);
-    private final LatLng ANVICS = new LatLng(55.780813, 37.605429);
-    private final LatLng MARKER1 = new LatLng(55.779516, 37.601032);
-    private final LatLng MARKER2 = new LatLng(55.781724, 37.598918);
     private final LatLng MARKER3 = new LatLng(55.779781, 37.609029);
-    private final LatLng MARKER4 = new LatLng(55.770781, 37.609223);
     private final int DEFAULT_MAP_ZOOM = 10;
+    private final String coffeeLatLng[] = {"38.961814, -77.036347", "38.923592, -76.975781", "38.910040, -76.971516",
+            "38.897782, -77.017332", "38.904939, -77.037914", "55.780813, 37.605429", "55.779516, 37.601032", "55.781724, 37.598918",
+            "55.779781, 37.609029", "55.770781, 37.609223"};
 
+    private ArrayList<Coffee> coffeeList = new ArrayList<>();
+
+    //map
     private Button mButtonPlusZoom;
     private Button mButtonMinusZoom;
     private Button mButtonMyLocate;
+    private GoogleMap mMap;
+    private LocationManager mLocationManager;
+    private boolean mPermissionDenied = false;
+    private LatLng mLastKnownLocation;
+
+    //preViewCardView
+    private View peakView;
+    private TextView tvCoffeName;
+    private TextView tvPreviewBottomJobTime;
+    private TextView tvPreviewBottomRateCount;
+    private TextView tvPreviewBottomRangeCount;
+    private TextView tv_coffee_name;
+    private TextView tv_coffee_address;
+    private TextView tv_coffee_phone_number;
+    private AppBarLayout appBarLayout;
+    private ImageView ivPreviewArrowTop;
+    private BottomSheetBehavior mBottomSheetBehavior;
+
     private RecyclerView recyclerview;
     private CoffeeAdapter mAdapter;
 
-    private GoogleMap mMap;
-    private View peakView;
-    private BottomSheetBehavior mBottomSheetBehavior;
-    private LocationManager mLocationManager;
-    private LatLng mLastKnownLocation;
-    private boolean mPermissionDenied = false;
-    private TextView tvCoffeName;
-    private AppBarLayout appBarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contain_main);
+        createCoffee();
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         mButtonPlusZoom = (Button) findViewById(R.id.btn_plus_zoom);
         mButtonPlusZoom.setOnClickListener(new View.OnClickListener() {
@@ -81,7 +96,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
                             new LatLng(mMap.getMyLocation().getLatitude(), mMap.getMyLocation().getLongitude()), DEFAULT_MAP_ZOOM));
                 }
-//                getLastKnownLocation();
             }
         });
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -112,6 +126,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
         tvCoffeName = (TextView) findViewById(R.id.tv_coffee_name);
+        ivPreviewArrowTop = (ImageView) findViewById(R.id.ivPreviewArrowTop);
         mapFragment.getMapAsync(this);
     }
 
@@ -122,13 +137,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         getLastKnownLocation();
         mMap.getUiSettings().setRotateGesturesEnabled(false);
         mMap.getUiSettings().setCompassEnabled(false);
-        mMap.getUiSettings().setMyLocationButtonEnabled(true);
-
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
                 peakView = findViewById(R.id.peakView);
-                mBottomSheetBehavior.setPeekHeight(peakView.getHeight() + tvCoffeName.getHeight());
+                mBottomSheetBehavior.setPeekHeight(peakView.getHeight() + tvCoffeName.getHeight() + ivPreviewArrowTop.getHeight());
+                initPeakView(marker);
                 if (mBottomSheetBehavior != null) {
                     switch (mBottomSheetBehavior.getState()) {
                         case (BottomSheetBehavior.STATE_HIDDEN):
@@ -179,7 +194,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         } else if (mMap != null) {
             // Access to the location has been granted to the app.
             mMap.setMyLocationEnabled(true);
-//            getLastKnownLocation();
         }
     }
 
@@ -215,14 +229,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 
-    void setMarkers() {
-        mMap.addMarker(new MarkerOptions().position(ANVICS).title("ANVICS"));
-        mMap.addMarker(new MarkerOptions().position(MARKER1));
-        mMap.addMarker(new MarkerOptions().position(MARKER2));
-        mMap.addMarker(new MarkerOptions().position(MARKER3));
-        mMap.addMarker(new MarkerOptions().position(MARKER4));
-    }
-
     private void getLastKnownLocation() {
         List<String> providers = mLocationManager.getProviders(false);
         Location bestLocation = null;
@@ -252,5 +258,47 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         } else {
             super.onBackPressed();
         }
+    }
+
+    void createCoffee() {
+        for (int i = 0; i < 10; i++) {
+            Coffee coffee = new Coffee();
+            coffee.setName("Coffee" + i);
+            coffee.setAdress("Улица Старая Басманная д." + i);
+            coffee.setNumber("+7 (903) 999 99 9" + i);
+            coffee.setJobTime("22-3" + i);
+            coffee.setRating(String.valueOf(i));
+            coffee.setRange(String.valueOf(i) + "000");
+            coffee.setCoordinate(coffeeLatLng[i]);
+            coffeeList.add(coffee);
+        }
+    }
+
+    void setMarkers() {
+        for (int i = 0; i < coffeeList.size(); i++) {
+            String[] latLng = coffeeList.get(i).getCoordinate().split(",");
+            double latitude = Double.parseDouble(latLng[0]);
+            double longitude = Double.parseDouble(latLng[1]);
+            LatLng coffeeLatLng = new LatLng(latitude, longitude);
+            mMap.addMarker(new MarkerOptions().position(coffeeLatLng).snippet(String.valueOf(i)));
+        }
+    }
+
+    void initPeakView(Marker marker) {
+        int id = Integer.parseInt(marker.getSnippet());
+        Coffee coffee = coffeeList.get(id);
+        tv_coffee_name = (TextView) findViewById(R.id.tv_coffee_name);
+        tv_coffee_name.setText(coffee.getName());
+        tv_coffee_address = (TextView) findViewById(R.id.tv_coffe_address);
+        tv_coffee_address.setText(coffee.getAdress());
+        tv_coffee_phone_number = (TextView) findViewById(R.id.tv_coffe_phone_number);
+        tv_coffee_phone_number.setText(coffee.getNumber());
+
+        tvPreviewBottomJobTime = (TextView) findViewById(R.id.tvPreviewBottomJobTime);
+        tvPreviewBottomJobTime.setText(coffee.getJobTime());
+        tvPreviewBottomRateCount = (TextView) findViewById(R.id.tvPreviewBottomRateCount);
+        tvPreviewBottomRateCount.setText(coffee.getRating());
+        tvPreviewBottomRangeCount = (TextView) findViewById(R.id.tvPreviewBottomRangeCount);
+        tvPreviewBottomRangeCount.setText(coffee.getRange());
     }
 }

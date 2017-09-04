b55c5f7863148a6ea819a30c4af48c1446c8da9c
sergey <kudishov@inbox.ru>
2017-09-04 17:37:29 +0300

    Authorization Init

--- app/src/main/java/com/example/adm1n/coffeescope/introduction/authorization/IntroductionAuthorizationFragment.java
+++ app/src/main/java/com/example/adm1n/coffeescope/introduction/authorization/IntroductionAuthorizationFragment.java
@@ -7,10 +7,21 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.adm1n.coffeescope.BaseFragment;
 import com.example.adm1n.coffeescope.R;
+import com.jakewharton.rxbinding2.widget.RxTextView;
+import com.jakewharton.rxbinding2.widget.TextViewAfterTextChangeEvent;
+
+import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 
 /**
  * Created by adm1n on 04.09.2017.
@@ -19,7 +30,13 @@ import com.example.adm1n.coffeescope.R;
 public class IntroductionAuthorizationFragment extends BaseFragment {
 
     private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPass;
     private EditText etEmail;
+    private EditText etPassword;
+
+    private Button btnRegistration;
+    private Button btnLogin;
+    private TextView tvForgotPassword;
 
     public static IntroductionAuthorizationFragment newInstance() {
 
@@ -40,14 +57,19 @@ public class IntroductionAuthorizationFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_introduction_authorization, null);
         textInputLayoutEmail = (TextInputLayout) view.findViewById(R.id.textInputLayoutEmail);
+        textInputLayoutPass = (TextInputLayout) view.findViewById(R.id.textInputLayoutPass);
         etEmail = (EditText) view.findViewById(R.id.etEmail);
+        etPassword = (EditText) view.findViewById(R.id.etPassword);
+        btnLogin = (Button) view.findViewById(R.id.btnAuthorizationLogin);
+        btnRegistration = (Button) view.findViewById(R.id.btnAuthorizationRegistration);
+        tvForgotPassword = (TextView) view.findViewById(R.id.tvForgotPassword);
         return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
+        setRxListener();
         etEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
@@ -61,5 +83,68 @@ public class IntroductionAuthorizationFragment extends BaseFragment {
                 textInputLayoutEmail.postInvalidate();
             }
         });
+
+        btnRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Переход на регистрацию", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Логинимся", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tvForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Восстанавливаем пароль", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setRxListener() {
+        Observable<Boolean> checkEmailField = RxTextView.afterTextChangeEvents(
+                etEmail)
+                .map(new Function<TextViewAfterTextChangeEvent, Boolean>() {
+                    @Override
+                    public Boolean apply(@NonNull TextViewAfterTextChangeEvent textViewAfterTextChangeEvent) throws Exception {
+                        String text = textViewAfterTextChangeEvent.view().getText().toString();
+                        if (textViewAfterTextChangeEvent.view().length() == 0) {
+                            return false;
+                        } else {
+                            return text.contains("@") && text.contains(".");
+                        }
+                    }
+                });
+
+        Observable<Boolean> checkPassField = RxTextView.afterTextChangeEvents(
+                etPassword)
+                .map(new Function<TextViewAfterTextChangeEvent, Boolean>() {
+                    @Override
+                    public Boolean apply(@NonNull TextViewAfterTextChangeEvent textViewAfterTextChangeEvent) throws Exception {
+                        return textViewAfterTextChangeEvent.view().length() >= 4;
+                    }
+                });
+
+        Observable.combineLatest(checkEmailField, checkPassField, new BiFunction<Boolean, Boolean, Boolean>() {
+            @Override
+            public Boolean apply(@NonNull Boolean aBoolean, @NonNull Boolean aBoolean2) throws Exception {
+                return aBoolean && aBoolean2;
+            }
+        }).subscribe(new Consumer<Boolean>() {
+            @Override
+            public void accept(@NonNull Boolean aBoolean) throws Exception {
+                if (aBoolean) {
+                    btnLogin.setEnabled(true);
+                } else {
+                    btnLogin.setEnabled(false);
+                }
+            }
+        });
+        btnLogin.setEnabled(false);
     }
 }
